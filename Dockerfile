a613c1ce5f4fd733b523c742db1d6a1edaa95667
asoluwaseun <soadesina71@student.lautech.edu.ng>
2019-10-19 10:11:23 +0100

    Getting Started with SPAs and GKE

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM mhart/alpine-node as app-build
+WORKDIR /usr/src/app
+
+COPY package.json ./
+RUN npm install
+
+COPY . ./
+RUN npm run build
+
+FROM nginx:alpine
+COPY --from=app-build /usr/src/app/build /usr/share/nginx/html
+
+EXPOSE 80
+CMD ["nginx", "-g","daemon off;"]   
--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './earth.gif';
+import hello from './hello.jpg';
 import './App.css';
+
+const style = {
+  backgroundImage: `url(${hello})`
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={style}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Docker!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/asoluwaseun"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
